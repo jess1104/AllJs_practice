7870f720b1632c9c3a0a026db1e5aa65458f90a4
Jess Wu <jess1104@hotmail.com.tw>
2021-08-31 23:27:43 +0800

    ES_MODULE

---
+++ ES_Module/export1.js
@@ -0,0 +1,9 @@
+let a = 1;
+let b = 2;
+let c = a+b;
+
+// 預設匯出
+export default {
+    a:5,
+    content:b,
+}
---
+++ ES_Module/index.js
@@ -0,0 +1,4 @@
+// 匯入
+import data from "./export1.js";
+// data是自己取的名字把export1.js的檔案內容帶進來
+console.log(data)
