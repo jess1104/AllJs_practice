7e1d4d80ba57812d1a1cf33fc72c73f07e854d1b
jess <jesswu@gmail.com>
2021-10-04 12:17:57 +0800

    左右輪播,還沒簡化函式

--- starter/script.js
+++ starter/script.js
@@ -68,3 +68,63 @@ btnScrollTo.addEventListener('click', function (e) {
 // page navigation(nav bar)
 
 document.querySelectorAll('.nav__link');
+
+// lazy loading
+// 抓到只有data-src的img
+const imgTargets = document.querySelectorAll('img[data-src]');
+// console.log(imgTargets);
+
+const loadImg = function (entires, obserrver) {
+  const [entry] = entires;
+  // console.log(entry);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider 輪播
+// 先抓到所有的圖
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.3)';
+slider.style.overflow = 'visible';
+
+// 這個是要與當下的index去作用
+let curSide = 0;
+// 當點到底就不能再繼續translate
+const maxSlide = slides.length;
+// 所有slides相連
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`)); // 0%,100%,200%,300%
+
+// 點擊下一張
+btnRight.addEventListener('click', function () {
+  // 做一個判斷當等於最後一張要返回
+  if (curSide === maxSlide - 1) {
+    return;
+    // curSide = 0;
+  } else {
+    curSide++;
+  }
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - curSide)}%)`)
+  ); // -100%,0%,100%,200%
+});
+
+// 點擊上一張
+btnLeft.addEventListener('click', function () {
+  if (curSide === 0) {
+    return;
+    // curSide = maxSlide - 1;
+  } else {
+    curSide--;
+  }
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - curSide)}%)`)
+  );
+});
